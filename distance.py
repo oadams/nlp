399bfdf6c6e6122d01a49254c1bf446028bcb08a
Oliver Adams <oliver.adams@gmail.com>
2014-02-19 12:49:01 +1100

    merged word error rate into distance module

--- distance.py
+++ distance.py
@@ -144,3 +144,10 @@ def cluster_alignment_errors(alignment):
         mistakes = ([],[])
 
     return newalign
+
+def word_error_rate(ref, hypo):
+    """Returns the word error rate of the supplied hypothesis with respect to
+    the reference string."""
+
+    min_edit_distance = min_edit_distance(ref, hypo)
+    return 100 * float(min_edit_distance) / len(ref)
--- wer.py
+++
@@ -1,8 +0,0 @@
-import distance
-
-def word_error_rate(ref, hypo):
-    """Returns the word error rate of the supplied hypothesis with respect to
-    the reference string."""
-
-    min_edit_distance = distance.min_edit_distance(ref, hypo)
-    return 100 * float(min_edit_distance) / len(ref)
