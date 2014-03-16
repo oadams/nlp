7cff4275ff6d208c91929f62d75376f99f3aa3bb
Oliver Adams <oliver.adams@gmail.com>
2014-03-16 11:17:10 +1100

    Committing the very start of some haskell LM code from a while back.

--- lm.hs
+++ lm.hs
@@ -1,6 +1,12 @@
 import qualified Data.Map as Map
 
+increment :: String -> Map.Map String Integer
+increment k map
+    | Map.member k map = Map.insert k ((Lookup k map) + 1)
+
 train :: String -> Map.Map String Double
+train (x:xs)
+    | Map.member x (train xs) = Map.insert x (Lookup x ((train xs) + 1)) (train xs)
 
 --train_unigram :: String -> Model
 
