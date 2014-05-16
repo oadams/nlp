44e5bba96fab7fe85d55a5af97a6737600045a87
Oliver Adams <oliver.adams@gmail.com>
2014-05-16 12:07:25 +1000

    added a number of lines argument to per.py

--- per.py
+++ per.py
@@ -6,12 +6,13 @@ parser = argparse.ArgumentParser(
         "transcription with respect to a supplied reference.")
 parser.add_argument("--ref", type=str)
 parser.add_argument("--hypo", type=str)
+parser.add_argument("--n", type=int)
 args = parser.parse_args()
 
 ref_lines = open(args.ref).readlines()
 hypo_lines = open(args.hypo).readlines()
 lines = zip(ref_lines, hypo_lines)
 distances = []
-for line in lines:
+for line in lines[:args.n]:
     distances.append(min_edit_distance(line[0].split(), line[1].split()))
 print float(sum(distances)) / sum([len(line.split()) for line in ref_lines])
